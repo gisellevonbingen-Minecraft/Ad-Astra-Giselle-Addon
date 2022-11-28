4be240ede0af61a703428141da5f8b9f9664a84f
gisellevonbingen <gisellevonbingen@gmail.com>
2022-11-29 01:00:55 +0900

    Adding Range Annotations

--- forge/src/main/java/ad_astra_giselle_addon/common/config/ItemsConfig.java
+++ forge/src/main/java/ad_astra_giselle_addon/common/config/ItemsConfig.java
@@ -2,6 +2,7 @@ package ad_astra_giselle_addon.common.config;
 
 import com.teamresourceful.resourcefulconfig.common.annotations.Category;
 import com.teamresourceful.resourcefulconfig.common.annotations.ConfigEntry;
+import com.teamresourceful.resourcefulconfig.common.annotations.LongRange;
 import com.teamresourceful.resourcefulconfig.common.config.EntryType;
 
 import earth.terrarium.botarium.api.fluid.FluidHooks;
@@ -15,7 +16,9 @@ public final class ItemsConfig
 	public static final String OXYGEN_CAN_ID = "oxygen_can";
 	public static final String OXYGEN_CAN_PREFIX = PREFIX + "." + OXYGEN_CAN_ID;
 	@ConfigEntry(id = OXYGEN_CAN_ID + ".fluid_capacity", type = EntryType.LONG, translation = OXYGEN_CAN_PREFIX + ".fluid_capacity")
+	@LongRange(min = 0, max = Long.MAX_VALUE)
 	public static long OXYGEN_CAN_FLUID_CAPACITY = FluidHooks.buckets(2);
 	@ConfigEntry(id = OXYGEN_CAN_ID + ".fluid_transfer", type = EntryType.LONG, translation = OXYGEN_CAN_PREFIX + ".fluid_transfer")
+	@LongRange(min = 0, max = Long.MAX_VALUE)
 	public static long OXYGEN_CAN_FLUID_TRANSFER = FluidHooks.toMillibuckets(256);
 }
