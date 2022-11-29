20bbd33f23e09908a0cb73d935e04b8a6f76a011
gisellevonbingen <gisellevonbingen@gmail.com>
2022-11-29 16:23:27 +0900

    Add config en_us LanguageProvider
    SQ

--- forge/src/main/java/ad_astra_giselle_addon/common/compat/mekanism/AddonMekanismConfig.java
+++ forge/src/main/java/ad_astra_giselle_addon/common/compat/mekanism/AddonMekanismConfig.java
@@ -13,10 +13,9 @@ public final class AddonMekanismConfig
 	public static final String ID = MekanismCompat.MODID;
 	public static final String PREFIX = CompatsConfig.PREFIX + "." + ID;
 	public static final String MODULES_ID = "modules";
-	public static final String MODULES_PREFIX = PREFIX + "." + MODULES_ID;
 
-	public static final String MODULES_SPACE_BREATHING_ID = "space_breathing";
-	public static final String MODULES_SPACE_BREATHING_PREFIX = MODULES_PREFIX + "." + MODULES_SPACE_BREATHING_ID;
+	public static final String MODULES_SPACE_BREATHING_ID = MODULES_ID + ".space_breathing";
+	public static final String MODULES_SPACE_BREATHING_PREFIX = PREFIX + "." + MODULES_SPACE_BREATHING_ID;
 	@ConfigEntry(id = MODULES_SPACE_BREATHING_ID + ".oxygen_duration", type = EntryType.INTEGER, translation = MODULES_SPACE_BREATHING_PREFIX + ".oxygen_duration")
 	@Comment("Duration of provided oxygen (oxygen provide interval)")
 	public static int MODULES_SPACE_BREATHING_OXYGEN_DURATION = 4;
@@ -27,14 +26,14 @@ public final class AddonMekanismConfig
 	@Comment("Energy usage per mb when produce Mekanism oxygen to player in water, rain")
 	public static int MODULES_SPACE_BREATHING_ENERGY_USING_PRODUCE = 200;
 
-	public static final String MODULES_SPACE_FIRE_PROOF_ID = "space_fire_proof";
-	public static final String SPACE_FIRE_MODULES_PROOF_PREFIX = MODULES_PREFIX + "." + MODULES_SPACE_FIRE_PROOF_ID;
+	public static final String MODULES_SPACE_FIRE_PROOF_ID = MODULES_ID + ".space_fire_proof";
+	public static final String SPACE_FIRE_MODULES_PROOF_PREFIX = PREFIX + "." + MODULES_SPACE_FIRE_PROOF_ID;
 	@ConfigEntry(id = MODULES_SPACE_FIRE_PROOF_ID + ".energy_using", type = EntryType.INTEGER, translation = SPACE_FIRE_MODULES_PROOF_PREFIX + ".energy_using")
 	@Comment("Energy usage when prevent fire every tick in Ad Astra hot planets (e.g. Venus, Mercury)")
 	public static int MODULES_SPACE_FIRE_PROOF_ENERGY_USING = 10;
 
-	public static final String MODULES_VENUS_ACID_PROOF_ID = "venus_acid_proof";
-	public static final String MODULES_VENUS_ACID_PROOF_PREFIX = MODULES_PREFIX + "." + MODULES_VENUS_ACID_PROOF_ID;
+	public static final String MODULES_VENUS_ACID_PROOF_ID = MODULES_ID + ".venus_acid_proof";
+	public static final String MODULES_VENUS_ACID_PROOF_PREFIX = PREFIX + "." + MODULES_VENUS_ACID_PROOF_ID;
 	@ConfigEntry(id = MODULES_VENUS_ACID_PROOF_ID + ".energy_using", type = EntryType.INTEGER, translation = MODULES_VENUS_ACID_PROOF_PREFIX + ".energy_using")
 	@Comment("Energy usage when prevent acid rain damage every tick in Ad Astra Venus")
 	public static int MODULES_VENUS_ACID_PROOF_ENERGY_USING = 10;
--- forge/src/main/java/ad_astra_giselle_addon/common/compat/pneumaticcraft/AddonPneumaticCraftConfig.java
+++ forge/src/main/java/ad_astra_giselle_addon/common/compat/pneumaticcraft/AddonPneumaticCraftConfig.java
@@ -13,10 +13,9 @@ public final class AddonPneumaticCraftConfig
 	public static final String ID = PneumaticCraftCompat.MODID;
 	public static final String PREFIX = CompatsConfig.PREFIX + "." + ID;
 	public static final String UPGRADE_ID = "modules";
-	public static final String UPGRADE_PREFIX = PREFIX + "." + UPGRADE_ID;
 
-	public static final String SPACE_BREATHING_ID = "space_breathing";
-	public static final String SPACE_BREATHING_PREFIX = UPGRADE_PREFIX + "." + SPACE_BREATHING_ID;
+	public static final String SPACE_BREATHING_ID = UPGRADE_ID + ".space_breathing";
+	public static final String SPACE_BREATHING_PREFIX = PREFIX + "." + SPACE_BREATHING_ID;
 	@ConfigEntry(id = SPACE_BREATHING_ID + ".oxygen_duration", type = EntryType.INTEGER, translation = SPACE_BREATHING_PREFIX + ".oxygen_duration")
 	@Comment("Duration of provided oxygen (oxygen provide interval)")
 	public static int SPACE_BREATHING_OXYGEN_DURATION = 4;
@@ -24,14 +23,14 @@ public final class AddonPneumaticCraftConfig
 	@Comment("Air usage when provide Ad Astra oxygen to player in space")
 	public static int SPACE_BREATHING_AIR_USING = 20;
 
-	public static final String SPACE_FIRE_PROOF_ID = "space_fire_proof";
-	public static final String SPACE_FIRE_PROOF_PREFIX = UPGRADE_PREFIX + "." + SPACE_FIRE_PROOF_ID;
+	public static final String SPACE_FIRE_PROOF_ID = UPGRADE_ID + ".space_fire_proof";
+	public static final String SPACE_FIRE_PROOF_PREFIX = PREFIX + "." + SPACE_FIRE_PROOF_ID;
 	@ConfigEntry(id = SPACE_FIRE_PROOF_ID + ".air_using", type = EntryType.INTEGER, translation = SPACE_FIRE_PROOF_PREFIX + ".air_using")
 	@Comment("Air usage when prevent fire every tick in Ad Astra hot planets (e.g. Venus, Mercury)")
 	public static int SPACE_FIRE_PROOF_AIR_USING = 5;
 
-	public static final String VENUS_ACID_PROOFID = "venus_acid_proof";
-	public static final String VENUS_ACID_PROOF_PREFIX = UPGRADE_PREFIX + "." + VENUS_ACID_PROOFID;
+	public static final String VENUS_ACID_PROOFID = UPGRADE_ID + ".venus_acid_proof";
+	public static final String VENUS_ACID_PROOF_PREFIX = PREFIX + "." + VENUS_ACID_PROOFID;
 	@ConfigEntry(id = VENUS_ACID_PROOFID + ".air_using", type = EntryType.INTEGER, translation = VENUS_ACID_PROOF_PREFIX + ".air_using")
 	@Comment("Air usage when prevent acid rain damage every tick in Ad Astra Venus")
 	public static int VENUS_ACID_PROOF_AIR_USING = 5;
