762bca62d35696465cb5414a0aa8b27f48c5586c
gisellevonbingen <gisellevonbingen@gmail.com>
2023-03-15 22:48:05 +0900

    Fix FluidHelper

--- fabric/src/main/java/ad_astra_giselle_addon/common/fluid/FluidHelperDelegate.java
+++ fabric/src/main/java/ad_astra_giselle_addon/common/fluid/FluidHelperDelegate.java
@@ -1,6 +1,17 @@
 package ad_astra_giselle_addon.common.fluid;
 
+import earth.terrarium.botarium.common.fluid.base.FluidHolder;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
+import net.minecraft.network.chat.Component;
+
 public class FluidHelperDelegate implements FluidHelper.Delegate
 {
+	@Override
+	public Component getDisplayName(FluidHolder fluid)
+	{
+		FluidVariant variant = FluidVariant.of(fluid.getFluid(), fluid.getCompound());
+		return FluidVariantAttributes.getName(variant);
+	}
 
 }
