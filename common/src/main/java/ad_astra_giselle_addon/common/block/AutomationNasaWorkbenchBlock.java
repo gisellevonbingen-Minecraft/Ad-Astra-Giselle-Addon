ff53041b01c290ec0c9bed5adba3083bf2478b19
gisellevonbingen <gisellevonbingen@gmail.com>
2023-06-16 13:26:02 +0900

    Fix gradle can't run

--- common/src/main/java/ad_astra_giselle_addon/common/block/AutomationNasaWorkbenchBlock.java
+++ common/src/main/java/ad_astra_giselle_addon/common/block/AutomationNasaWorkbenchBlock.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import ad_astra_giselle_addon.common.block.entity.AutomationNasaWorkbenchBlockEntity;
 import ad_astra_giselle_addon.common.util.TranslationUtils;
+import earth.terrarium.ad_astra.AdAstra;
 import earth.terrarium.ad_astra.common.block.machine.AbstractMachineBlock;
-import earth.terrarium.ad_astra.common.registry.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
@@ -16,7 +17,7 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class AutomationNasaWorkbenchBlock extends AbstractMachineBlock
 {
-	public static final Component TOOLTIP = TranslationUtils.getItemTooltip(ModBlocks.NASA_WORKBENCH.getId());
+	public static final Component TOOLTIP = TranslationUtils.getItemTooltip(new ResourceLocation(AdAstra.MOD_ID, "nasa_workbench"));
 
 	public AutomationNasaWorkbenchBlock(Properties properties)
 	{
