f97c6d48c2abb32d1acbdfce49590dc553594312
gisellevonbingen <gisellevonbingen@gmail.com>
2024-01-17 15:14:10 +0900

    Update FuelLoaderScreen.java

--- common/src/main/java/ad_astra_giselle_addon/client/screen/FuelLoaderScreen.java
+++ common/src/main/java/ad_astra_giselle_addon/client/screen/FuelLoaderScreen.java
@@ -1,11 +1,8 @@
 package ad_astra_giselle_addon.client.screen;
 
-import java.util.List;
-
 import ad_astra_giselle_addon.common.AdAstraGiselleAddon;
 import ad_astra_giselle_addon.common.block.entity.FuelLoaderBlockEntity;
 import ad_astra_giselle_addon.common.menu.FuelLoaderMenu;
-import ad_astra_giselle_addon.common.util.TranslationUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -19,11 +16,6 @@ public class FuelLoaderScreen extends AddonMachineScreen<FuelLoaderMenu, FuelLoa
 	public FuelLoaderScreen(FuelLoaderMenu menu, Inventory inventory, Component title)
 	{
 		super(menu, inventory, title, TEXTURE, STEEL_SLOT, WIDTH, HEIGHT);
-	}
-
-	public List<Component> getFluidBarTooltip()
-	{
-		return TranslationUtils.fluid(this.entity.getFluidContainer());
 	}
 
 }
