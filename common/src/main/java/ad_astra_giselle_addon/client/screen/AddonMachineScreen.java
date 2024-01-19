b3a8ab1d8e0065c86b388adfb1eee66dfdae49cd
gisellevonbingen <gisellevonbingen@gmail.com>
2024-01-20 03:16:53 +0900

    Rework working area Button

--- common/src/main/java/ad_astra_giselle_addon/client/screen/AddonMachineScreen.java
+++ common/src/main/java/ad_astra_giselle_addon/client/screen/AddonMachineScreen.java
@@ -1,8 +1,5 @@
 package ad_astra_giselle_addon.client.screen;
 
-import java.awt.Rectangle;
-import java.text.NumberFormat;
-
 import org.jetbrains.annotations.Nullable;
 
 import ad_astra_giselle_addon.client.mixin.ad_astra.ConfigurationWidgetAccessor;
@@ -11,14 +8,16 @@ import ad_astra_giselle_addon.common.block.entity.IWorkingAreaBlockEntity;
 import ad_astra_giselle_addon.common.compat.CompatibleManager;
 import ad_astra_giselle_addon.common.network.AddonNetwork;
 import ad_astra_giselle_addon.common.network.WorkingAreaVisibleMessage;
+import earth.terrarium.adastra.client.components.PressableImageButton;
 import earth.terrarium.adastra.client.components.machines.FluidBarWidget;
+import earth.terrarium.adastra.client.components.machines.OptionsBarWidget.Builder;
 import earth.terrarium.adastra.client.screens.base.MachineScreen;
+import earth.terrarium.adastra.client.utils.GuiUtils;
 import earth.terrarium.adastra.common.blockentities.base.ContainerMachineBlockEntity;
 import earth.terrarium.adastra.common.menus.base.MachineMenu;
 import earth.terrarium.adastra.common.menus.configuration.FluidConfiguration;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -27,8 +26,7 @@ import net.minecraft.world.phys.AABB;
 
 public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>, BLOCK_ENTITY extends ContainerMachineBlockEntity> extends MachineScreen<MENU, BLOCK_ENTITY>
 {
-	public static final String WORKINGAREA_TEXT_PREFIX = ctl("workingarea.");
-	public static final String WORKINGAREA_TEXT_KEY = WORKINGAREA_TEXT_PREFIX + "text";
+	public static final Component WORKINGAREA_TEXT = Component.translatable(ctl("workingarea")).withStyle(ChatFormatting.GRAY);
 
 	public static String ctl(String path)
 	{
@@ -41,7 +39,7 @@ public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>,
 	}
 
 	private boolean cachedWorkingAreaVisible;
-	private Button workingAreaVisibleButton;
+	protected PressableImageButton workingAreaVisibleButton;
 
 	public AddonMachineScreen(MENU menu, Inventory inventory, Component title, ResourceLocation texture, ResourceLocation slotTexture, int width, int height)
 	{
@@ -55,12 +53,44 @@ public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>,
 
 		if (this.hasWorkingArea())
 		{
-			this.workingAreaVisibleButton = this.addRenderableWidget(new Button.Builder(Component.empty(), this::onChangeModeButtonClick).bounds(this.leftPos, this.topPos - 20, 20, 20).build());
-
 			boolean visible = this.isWorkingAreaVisible();
 			this.cachedWorkingAreaVisible = visible;
-			this.setWorkingAreaVisible(visible);
-			this.refreshWorkingAreaVisibleButtonMessage();
+			this.updateWorkingAreaVisibleButton();
+		}
+
+	}
+
+	@Override
+	public Builder createOptionsBar()
+	{
+		Builder builder = super.createOptionsBar();
+
+		if (this.entity instanceof IWorkingAreaBlockEntity blockEntity)
+		{
+			this.workingAreaVisibleButton = this.createWorkingAreaShowMode();
+			builder.addElement(0, this.workingAreaVisibleButton);
+		}
+		else
+		{
+			this.workingAreaVisibleButton = null;
+		}
+
+		return builder;
+	}
+
+	@Nullable
+	public PressableImageButton createWorkingAreaShowMode()
+	{
+		if (this.entity instanceof IWorkingAreaBlockEntity blockEntity)
+		{
+			return new PressableImageButton(0, 0, 18, 18, 0, 0, 18, blockEntity.isWorkingAreaVisible() ? GuiUtils.SHOW_BUTTON : GuiUtils.HIDE_BUTTON, 18, 54, button ->
+			{
+				this.setWorkingAreaVisible(!this.isWorkingAreaVisible());
+			}, WORKINGAREA_TEXT);
+		}
+		else
+		{
+			return null;
 		}
 
 	}
@@ -70,15 +100,6 @@ public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>,
 	{
 		super.render(guiGraphics, mouseX, mouseY, delta);
 		this.updateWorkingAreaVisibleButton();
-	}
-
-	public void resizeWorkingAreaVisibleButton()
-	{
-		Button workingAreaVisibleButton = this.getWorkingAreaVisibleButton();
-		int messageWidth = workingAreaVisibleButton.getHeight();
-		messageWidth = Math.max(messageWidth, this.font.width(this.getWorkingAreaVisibleText(true)));
-		messageWidth = Math.max(messageWidth, this.font.width(this.getWorkingAreaVisibleText(false)));
-		workingAreaVisibleButton.setWidth(messageWidth + 8);
 	}
 
 	public boolean hasWorkingArea()
@@ -117,11 +138,6 @@ public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>,
 
 	}
 
-	public void onChangeModeButtonClick(net.minecraft.client.gui.components.Button button)
-	{
-		this.setWorkingAreaVisible(!this.isWorkingAreaVisible());
-	}
-
 	public void updateWorkingAreaVisibleButton()
 	{
 		boolean next = this.isWorkingAreaVisible();
@@ -129,55 +145,9 @@ public abstract class AddonMachineScreen<MENU extends MachineMenu<BLOCK_ENTITY>,
 		if (this.cachedWorkingAreaVisible != next)
 		{
 			this.cachedWorkingAreaVisible = next;
-			this.refreshWorkingAreaVisibleButtonMessage();
+			this.workingAreaVisibleButton.setTexture(next ? GuiUtils.SHOW_BUTTON : GuiUtils.HIDE_BUTTON);
 		}
 
-	}
-
-	public void refreshWorkingAreaVisibleButtonMessage()
-	{
-		Component message = this.getWorkingAreaVisibleText(this.cachedWorkingAreaVisible);
-		this.getWorkingAreaVisibleButton().setMessage(message);
-		this.resizeWorkingAreaVisibleButton();
-	}
-
-	public Button getWorkingAreaVisibleButton()
-	{
-		return this.workingAreaVisibleButton;
-	}
-
-	protected void drawWorkingAreaText(GuiGraphics guiGraphics, @Nullable AABB workingArea, AbstractWidget left)
-	{
-		if (workingArea == null)
-		{
-			return;
-		}
-
-		Component workingAreaText = this.getWorkingAreaBoundsText(workingArea);
-
-		int sideWidth = 1;
-		int sidePadding = 2;
-		int workingAreaWidth = this.font.width(workingAreaText) + (sidePadding * 2);
-		int workingAreaHeight = 11;
-		int workingAreaLeft = left.getX() + left.getWidth() - this.leftPos;
-		int workingAreaTop = this.topPos - workingAreaHeight;
-
-		GuiUtils2.drawWorkingAreaBackground(guiGraphics, new Rectangle(workingAreaLeft, workingAreaTop, workingAreaWidth, 11));
-		guiGraphics.drawString(this.font, workingAreaText, workingAreaLeft + sideWidth + sidePadding, workingAreaTop + 2, 0x303030);
-	}
-
-	protected Component getWorkingAreaBoundsText(AABB workingArea)
-	{
-		NumberFormat numberInstance = NumberFormat.getNumberInstance();
-		numberInstance.setMaximumFractionDigits(2);
-		Component workingAreaText = Component.translatable(WORKINGAREA_TEXT_KEY, numberInstance.format(workingArea.getXsize()), numberInstance.format(workingArea.getYsize()), numberInstance.format(workingArea.getZsize()));
-		return workingAreaText;
-	}
-
-	protected Component getWorkingAreaVisibleText(boolean visible)
-	{
-		String method = visible ? "hide" : "show";
-		return Component.translatable(WORKINGAREA_TEXT_PREFIX + method);
 	}
 
 	public int getTextColour()
