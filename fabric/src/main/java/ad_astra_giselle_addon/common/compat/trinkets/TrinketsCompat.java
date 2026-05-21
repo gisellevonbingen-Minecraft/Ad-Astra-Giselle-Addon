8e82188f287ab74a0dfa60f91c71534429c60cf3
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-05-21 23:13:00 +0900

    Trinkets

---
+++ fabric/src/main/java/ad_astra_giselle_addon/common/compat/trinkets/TrinketsCompat.java
@@ -0,0 +1,21 @@
+package ad_astra_giselle_addon.common.compat.trinkets;
+
+import ad_astra_giselle_addon.common.compat.CompatibleMod;
+
+public class TrinketsCompat extends CompatibleMod
+{
+	public static final String MOD_ID = "trinkets";
+
+	@Override
+	public String getModId()
+	{
+		return MOD_ID;
+	}
+
+	@Override
+	protected void onLoad()
+	{
+
+	}
+
+}
--- fabric/src/main/java/ad_astra_giselle_addon/common/entity/LivingHelperDelegate.java
+++ fabric/src/main/java/ad_astra_giselle_addon/common/entity/LivingHelperDelegate.java
@@ -1,6 +1,52 @@
 package ad_astra_giselle_addon.common.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ad_astra_giselle_addon.common.compat.CompatibleManagerDelegate;
+import ad_astra_giselle_addon.common.compat.trinkets.TrinketsHelper;
+import ad_astra_giselle_addon.common.item.ItemStackConsumers;
+import ad_astra_giselle_addon.common.item.ItemStackReference;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
 public class LivingHelperDelegate implements LivingHelper.Delegate
 {
+	@Override
+	public List<ItemStackReference> getExtraSlotEquipments(LivingEntity living)
+	{
+		List<ItemStackReference> list = new ArrayList<>();
+		list.addAll(this.getTrinketsStacks(living));
+		return list;
+	}
+
+	public List<ItemStackReference> getTrinketsStacks(LivingEntity living)
+	{
+		List<ItemStackReference> list = new ArrayList<>();
+
+		if (CompatibleManagerDelegate.TRINKETS.isLoaded())
+		{
+			Container container = TrinketsHelper.getEquippedTrinkets(living);
+
+			if (container != null)
+			{
+				for (int i = 0; i < container.getContainerSize(); i++)
+				{
+					ItemStack item = container.getItem(i);
+
+					if (!item.isEmpty())
+					{
+						list.add(new ItemStackReference(item, ItemStackConsumers.index(i, container::setItem)));
+					}
+
+				}
+
+			}
+
+		}
+
+		return list;
+	}
 
 }
